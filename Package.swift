2ec546bb094fc3b3a44c0d6e7dc3a59038657ade
unouprime.oder@outlook.com <unouprime.oder@outlook.com>
2020-12-02 07:53:07 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,21 +11,27 @@ let package = Package(
       name: "EpubKit.swift",
       targets: ["EpubKit.swift"]
     ),
+    .executable(
+      name: "EpubKit",
+      targets: ["CommandLineTool"]
+    ),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
+    .package(name: "Plot", url: "https://github.com/johnsundell/plot.git", from: "0.8.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "EpubKit.swift",
-      dependencies: []
+      dependencies: ["Plot"]
     ),
-    .testTarget(
-      name: "EpubKit.swiftTests",
-      dependencies: ["EpubKit.swift"]
+    .target(
+      name: "CommandLineTool",
+      dependencies: ["Plot"]
     ),
+    // .testTarget(name: "EpubKit.swiftTests", dependencies: ["EpubKit.swift"]),
   ]
 )
