36dfd84be56eecfbbb3e5d2fb742089c995499e3
unouprime.oder@outlook.com <unouprime.oder@outlook.com>
2020-12-02 08:03:05 +0800

    formatted

--- Package_2.swift
+++ Package_2.swift
@@ -14,9 +14,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EpubKit.swift",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "EpubKit.swiftTests",
-            dependencies: ["EpubKit.swift"]),
+            dependencies: ["EpubKit.swift"]
+        ),
     ]
 )
